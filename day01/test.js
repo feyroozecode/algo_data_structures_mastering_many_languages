c480c21b6ff196308232a5fe9f901c79fadfb406
ibrahmad <ibrahmad18@gmail.com>
2023-12-16 08:31:21 +0100

    adding binary search algo

---
+++ day01/test.js
@@ -0,0 +1,15 @@
+
+function addArrays(a, b){
+    let s = 0
+
+    let array = []
+
+    for (let i =a ; i <= b; i++){
+        s += array[i];
+    }   
+
+    return s;
+}
+
+console.log(addArrays(4, 7) .toString() );
+
---
+++ day03/binary_search.py
@@ -0,0 +1,29 @@
+# Binary Search 
+def binary_search(arr, target):
+    low, high = 0, len(arr) -1
+    
+    while low <= high :
+        mid = (low + high ) // 2
+        
+        if arr [mid] == target:
+            return mid
+        
+        elif arr[mid] < target:
+          low = mid + 1
+        else :
+            high = mid -1
+        
+    return -1
+
+ # Test
+ 
+mArray = [ 4, 5, 7 , 9 , 2 , 12]
+x = int(input("Enter the array you wan to search : "))
+
+result = binary_search(mArray, x)
+
+if result != -1:
+    print("Element is present at index : "+ str(result))
+else:
+    print(f"{x} Not found  on array")
+    
