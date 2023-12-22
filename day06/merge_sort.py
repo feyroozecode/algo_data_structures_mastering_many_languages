463bcc12fcea0b854ab54a3b8fc9c38730e3c5c2
ibrahmad <ibrahmad18@gmail.com>
2023-12-22 21:46:55 +0100

    adding merge sort

---
+++ day06/merge_sort.py
@@ -0,0 +1,38 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort the two halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # Merge the sorted halves
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Copy any remaining elements from left and right (if any)
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example usage:
+my_array = [64, 34, 25, 12, 22, 11, 90]
+merge_sort(my_array)
+
+print("Sorted array:", my_array)
